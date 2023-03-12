c0c7f806b91d46547b1f893988c8c33f680fa158
stefan_pfleger <stefan.pfleger@stud-fh.campuswien.ac.at>
2023-03-12 17:48:11 +0100

    Initial Project creation

---
+++ src/test/java/at/fhcampuswien/carrental/carrentalservice/CarRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhcampuswien.carrental.carrentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
